1ded482e00da82f02ffaba3ab1c586842835c928
Pobarul-islam <pobarul3@gmail.com>
2022-07-05 19:54:05 +0600

    react native task manager started and some code added

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './Components/Task';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <View style={styles.taskWrapper}>
+
+        <Text style={styles.sectionTitle}>Today's Task</Text>
+        <View  styles={styles.items}>
+
+          {/* This is the task */}
+          <Task text={'Task 1'}></Task>
+          <Task text={'Task 2'}></Task>
+
+
+        </View>
+      </View>
+      
     </View>
   );
 }
@@ -13,8 +27,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
+   
   },
+  taskWrapper: {
+    paddingTop: 80,
+    paddingHorizontal:20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  }
 });
